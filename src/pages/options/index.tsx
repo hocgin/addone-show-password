2f7df279e7c7fc8b4f41d7d56e5f93c5e69bfe05
hocgin <hocgin@gmail.com>
2023-03-11 15:28:10 +0800

    更新

--- src/pages/options/index.tsx
+++ src/pages/options/index.tsx
@@ -3,6 +3,7 @@ import styles from './index.less';
 import {useMount, useNetwork} from 'ahooks';
 import {HookService} from "@/services/hook";
 import Config from "@/config";
+import {Loading} from "@hocgin/ui";
 
 export default () => {
   let {online} = useNetwork();
@@ -11,6 +12,7 @@ export default () => {
   });
   if (online) {
     window.location.href = `https://logspot.hocgin.top/${Config.getProjectId()}`;
+    return <Loading/>
   }
   return <div className={styles.options}>
     <Settings/>
