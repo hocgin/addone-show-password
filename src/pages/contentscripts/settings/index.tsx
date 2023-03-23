a15f183a0080e7f2c5e7edbcc63be2f10fbb8497
hocgin <hocgin@gmail.com>
2023-03-24 07:59:39 +0800

    修复一些网站无法使用

--- src/pages/contentscripts/settings/index.tsx
+++ src/pages/contentscripts/settings/index.tsx
@@ -1,14 +1,12 @@
 import ReactDOM from "react-dom";
 import {Settings} from "@/pages/options/settings";
 
-document.addEventListener("DOMContentLoaded", () => {
-  let elementId = 'addone-show-password';
+let elementId = 'addone-show-password';
+document.addEventListener("DOMContentLoaded", () => new MutationObserver(() => {
   let element = document.getElementById(elementId);
   console.log('element', element);
   if (!element) return;
-  new MutationObserver(() => {
-    let newElement = document.createElement('div');
-    ReactDOM.render(<Settings/>, newElement, () => element!.replaceWith(newElement));
-  }).observe(element, {attributes: true, childList: true, subtree: true});
-});
+  let newElement = document.createElement('div');
+  ReactDOM.render(<Settings/>, newElement, () => element!.replaceWith(newElement));
+}).observe(document.body, {childList: true, subtree: true}));
 
